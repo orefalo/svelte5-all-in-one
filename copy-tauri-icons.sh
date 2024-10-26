4ace747c20e6faefec454776b31b49f83426e584
Mark Burggraf <markb@mantisbible.com>
2024-10-26 15:47:02 -0700

    tauri-icons

---
+++ copy-tauri-icons.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create the src-tauri/icons directory if it doesn't exist
+mkdir -p src-tauri/icons
+
+# Copy and rename the required icons
+cp static/icon-1024.png src-tauri/icons/icon.png
+cp static/icon-32.png src-tauri/icons/32x32.png
+cp static/icon-128.png src-tauri/icons/128x128.png
+cp static/icon-128.png src-tauri/icons/128x128@2x.png
+cp static/icon-256.png src-tauri/icons/icon.icns
+cp static/icon-256.png src-tauri/icons/icon.ico
+
+echo "Icons copied successfully for Tauri!"
---
+++ setup-apps.sh
@@ -0,0 +1,3 @@
+bunx cap add android
+bunx cap add ios
+bunx cap sync
