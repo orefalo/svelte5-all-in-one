ef59f324dee92bb7afb6ca405bc00753d9547ac0
Mark Burggraf <markb@mantisbible.com>
2024-10-28 10:42:01 -0700

    0.2.0

---
+++ release.sh
@@ -0,0 +1,6 @@
+git checkout main
+git pull origin main
+git checkout release
+git merge main
+git push origin release
+git checkout main
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+
+	function handleClick() {
+		const message = document.getElementById('message');
+		if (message) {
+			message.textContent = 'You clicked the button.';
+		}
+		const title = document.getElementById('title');
+		if (title) {
+			title.textContent = 'Goodbye, Tauri!';
+		}
+	}
 </script>
 
-<Button onclick={() => alert('shadcn-svelte is working')}>Click me</Button>
+<div class="flex h-screen flex-col items-center justify-center">
+	<Button onclick={handleClick}>Click me</Button>
+	<div>
+		<h1 id="title">Hello, Tauri!</h1>
+		<div id="message"></div>
+	</div>
+</div>
